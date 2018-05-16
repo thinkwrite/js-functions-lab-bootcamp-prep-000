718dc8c4bf83d9ca88c94b90c19e36dafceccd74
Kevin Joseph Abalos <kevinabalos@gmail.com>
2018-05-16 05:22:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!";
+  return "Happy holidays, ${name}";
 }
-
-happyHolidaysTo(you);
 
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!";
